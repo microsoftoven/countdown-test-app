dc78017c235554243094a7a5796c02ce64d57cb5
microsoftoven <ash@toast.codes>
2021-01-12 19:04:41 -0600

    feat: setting up auth routes and passportJS with Google OAuth

---
+++ routes/authRoutes.js
@@ -0,0 +1,32 @@
+const passport = require('passport');
+
+module.exports = (app) => {
+    app.get('/', (req, res) => {
+        res.send({ hello: 'world!' });
+    });
+
+    app.get(
+        '/auth/google',
+        passport.authenticate('google', {
+            scope: ['profile', 'email'],
+        })
+    );
+
+    app.get(
+        '/auth/google/callback',
+        passport.authenticate('google'),
+        (req, res) => {
+            res.redirect('/surveys');
+        }
+    );
+
+    app.get('/api/logout', (req, res) => {
+        req.logout();
+        res.redirect('/');
+    });
+
+    app.get('/api/current_user', (req, res) => {
+        const data = req.user ? req.user : { _id: null };
+        res.send(data);
+    });
+};
