7aec4b8dc51dda97ca378601eda6ffd91043f17f
aduhautois <aduhautois@flyinghippo.com>
2021-01-21 01:09:24 -0600

    fix: logout page height

--- client/src/components/Logout/index.tsx
+++ client/src/components/Logout/index.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
-import { Page } from '../_layout/Page';
+import { PageAlignCenter } from '../_layout/Page';
 import { VideoEmbed } from '../_ui/VideoEmbed';
 
 interface Props {}
 
 export const Logout: React.FC<Props> = () => {
     return (
-        <div data-testid='logout-page'>
-            <Page>
+        <PageAlignCenter>
+            <div data-testid='logout-page'>
                 <p>Byeee!</p>
 
                 <br />
 
                 <VideoEmbed
                     title='Bacon Pancakes'
-                    url='https://www.youtube.com/embed/0_EW8aNgKlA'
+                    url='https://www.youtube.com/embed/cUYSGojUuAU'
                 />
-            </Page>
-        </div>
+            </div>
+        </PageAlignCenter>
     );
 };
--- client/src/components/_layout/Footer/styles.ts
+++ client/src/components/_layout/Footer/styles.ts
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 
 export const StyledFooter = styled.footer`
-    padding: 20px;
+    padding: 60px 20px 20px;
 
     @media screen and (min-width: 768px) {
-        padding: 30px;
+        padding: 60px 30px 30px;
     }
 `;
